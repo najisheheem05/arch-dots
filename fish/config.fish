8bedaadd01893ec468947c73e4690ed4395acb5f
najisheheem05 <najisheheem05@gmail.com>
2025-12-23 00:37:30 +0530

    freq backup of dots

--- fish/config.fish
+++ fish/config.fish
@@ -1,49 +1,65 @@
+# 1. SETUP PATH FIRST (Crucial for zoxide/pipx tools)
+# Created by `pipx`
+set PATH $PATH /home/zero/.local/bin
+
 if status is-interactive
-    # Starship custom prompt
+    # --- 2. CORE TOOLS ---
+
+    # Starship Prompt
     starship init fish | source
 
-    # Direnv + Zoxide
-    command -v direnv &> /dev/null && direnv hook fish | source
-    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
-
-    # Better ls
-    alias ls='eza --icons --group-directories-first -1'
-
-    # Abbrs
-    abbr lg 'lazygit'
-    abbr gd 'git diff'
-    abbr ga 'git add .'
-    abbr gc 'git commit -am'
-    abbr gl 'git log'
-    abbr gs 'git status'
-    abbr gst 'git stash'
-    abbr gsp 'git stash pop'
-    abbr gp 'git push'
-    abbr gpl 'git pull'
-    abbr gsw 'git switch'
-    abbr gsm 'git switch main'
-    abbr gb 'git branch'
-    abbr gbd 'git branch -d'
-    abbr gco 'git checkout'
-    abbr gsh 'git show'
-
-    abbr l 'ls'
-    abbr ll 'ls -l'
-    abbr la 'ls -a'
-    abbr lla 'ls -la'
-
-    # Custom colours
-    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
-
-    # For jumping between prompts in foot terminal
-    function mark_prompt_start --on-event fish_prompt
-        echo -en "\e]133;A\e\\"
+    # Zoxide (The Teleporter)
+    if command -v zoxide &>/dev/null
+        zoxide init fish | source
     end
-end
 
-# Created by `pipx` on 2025-11-18 05:15:54
-set PATH $PATH /home/naji/.local/bin
+    # Direnv
+    if command -v direnv &>/dev/null
+        direnv hook fish | source
+    end
 
-if not set -q SSH_AUTH_SOCK
-    eval (ssh-agent -c) >/dev/null
+    # --- 3. ALIASES & EYE CANDY ---
+
+    # Short Aliases and Abbreviations
+    abbr c clear
+
+    # Modern replacements
+    alias ls='eza --icons --group-directories-first -1'
+    alias ll='eza --icons --group-directories-first -l'
+
+    # alias lgt='eza --tree -l --icons --group-directories-first --git'
+    alias la='eza -la --icons --group-directories-first'
+    alias lt='eza --tree --icons --group-directories-first'
+    alias l='eza -lhF --icons --group-directories-first'
+
+    # Colorful tree view with 2 levels by default
+    alias tree='eza --tree --icons --group-directories-first -L 2'
+    #alias cat='bat'
+
+    # Safety nets
+    alias rm='rm -i'
+    alias cp='cp -i'
+    alias mv='mv -i'
+
+    # Git Abbreviations
+    abbr lg lazygit
+    abbr ga 'git add .'
+    abbr gc 'git commit -m'
+    abbr gp 'git push'
+    abbr gs 'git status'
+
+    # --- 4. INTEGRATIONS ---
+
+    # YAZI WRAPPER (Fixes "cd" on exit)
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    # Custom Caelestia Colors
+    command cat ~/.local/state/caelestia/sequences.txt 2>/dev/null
 end
